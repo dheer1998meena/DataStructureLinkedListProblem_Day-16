571c22c8b98388cb384a17c5e5e93d14f43c1831
dheer meena <dheer1998meena@gmail.com>
2020-10-24 16:15:02 +0530

    [Dheer] Insert the node at the begining of LinkedList

--- DataStructureLinkedListProblem_Day-16/Program.cs
+++ DataStructureLinkedListProblem_Day-16/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressBook.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Dheer Singh Meena"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
 
 namespace DataStructureLinkedListProblem_Day_16
 {
@@ -8,9 +14,15 @@ namespace DataStructureLinkedListProblem_Day_16
         {
             Console.WriteLine("Welcome to Data Structure Linked List problem");
             LinkedList linkedList = new LinkedList();
+            // UC1 Creating Linked list insert the node at last
             linkedList.InsertLast(56);
             linkedList.InsertLast(30);
             linkedList.InsertLast(70);
+            linkedList.DisplayList();
+            // UC2 Inserting the node at the begining.
+            linkedList.InsertFront(70);
+            linkedList.InsertFront(30);
+            linkedList.InsertFront(56);
             linkedList.DisplayList();
         }
     }
